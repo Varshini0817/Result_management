7fa23a09e9c9a3208629f1af31570ff91cd1b350
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-12-20 12:20:31 +0530

    deployment

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.use('/api/staff',staffRoute);
 app.use('/api/student', studentRoute);
 app.use("/api/result",resultRoute);
 
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>
 {
